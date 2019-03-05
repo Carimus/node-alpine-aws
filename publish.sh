c111d83eab6af395939b3c0e287c6dccd4c260db
Brandon Phillips <bericp1@gmail.com>
2019-03-04 18:12:20 -0700

    update publish script to tag images with git commits; split up package install from aws cli install; install wait-for-it

--- publish.sh
+++ publish.sh
@@ -2,28 +2,52 @@
 
 set -e
 
-echo "Building image for node8:"
+GIT_STATUS="$(git status -s)"
+GIT_SHORT_COMMIT="$(git rev-parse --short HEAD)"
 
-docker build -t carimus/node-alpine-aws:node8 \
-  -f ./node8/Dockerfile \
+REPO_NAME="carimus/node-alpine-aws"
+NODE8_TAG="node8"
+NODE10_TAG="node10"
+
+if [[ -n "$GIT_STATUS" ]]; then
+  echo "There are untracked changes or the working tree is dirty."
+  echo "The image is tagged with the short git commit so please commit any changes before publishing."
+  exit 1
+fi
+
+echo "Will build images with the following tags:"
+echo -e "Based on node:10-alpine:\t$NODE10_TAG, latest, $GIT_SHORT_COMMIT, $GIT_SHORT_COMMIT-$NODE10_TAG"
+echo -e "Based on node:8-alpine:\t$NODE8_TAG, $GIT_SHORT_COMMIT-$NODE8_TAG"
+echo
+
+echo "Based on node:10-alpine (default image):"
+
+docker build -t $REPO_NAME:$NODE10_TAG \
+  -t $REPO_NAME:latest \
+  -t $REPO_NAME:$GIT_SHORT_COMMIT \
+  -t $REPO_NAME:$GIT_SHORT_COMMIT-$NODE10_TAG \
+  -f ./node10/Dockerfile \
   .
 
 echo
 
-echo "Building image for node10 (default image):"
+echo "Based on node:8-alpine:"
 
-docker build -t carimus/node-alpine-aws:node10 \
-  -t carimus/node-alpine-aws:latest \
-  -f ./node10/Dockerfile \
+docker build -t $REPO_NAME:$NODE8_TAG \
+  -t $REPO_NAME:$GIT_SHORT_COMMIT-$NODE8_TAG \
+  -f ./node8/Dockerfile \
   .
 
 echo
 
 echo "Pushing images to docker hub:"
 
-docker push carimus/node-alpine-aws:node8
-docker push carimus/node-alpine-aws:node10
-docker push carimus/node-alpine-aws:latest
+docker push $REPO_NAME:$NODE10_TAG
+docker push $REPO_NAME:latest
+docker push $REPO_NAME:$GIT_SHORT_COMMIT
+docker push $REPO_NAME:$GIT_SHORT_COMMIT-$NODE10_TAG
+docker push $REPO_NAME:$NODE8_TAG
+docker push $REPO_NAME:$GIT_SHORT_COMMIT-$NODE8_TAG
 
 echo
 
