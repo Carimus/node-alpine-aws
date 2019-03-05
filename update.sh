9fb91bdde32a4f7694318377c4588cc8515b11ac
Brandon Phillips <bericp1@gmail.com>
2019-03-04 18:16:44 -0700

    fix image to install curl

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ set -e
 mkdir -p ./node8/
 mkdir -p ./node10/
 
-sed s/node:alpine/node:8-alpine/ < ./Dockerfile.template > ./node8/Dockerfile
-sed s/node:alpine/node:10-alpine/ < ./Dockerfile.template > ./node10/Dockerfile
+sed s/__BASE_IMAGE__/node:8-alpine/ < ./Dockerfile.template > ./node8/Dockerfile
+sed s/__BASE_IMAGE__/node:10-alpine/ < ./Dockerfile.template > ./node10/Dockerfile
