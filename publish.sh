263853edbbd4bfd0b396c4c915947bfca4794d88
Brennon Lee <brennonl@carimus.com>
2020-09-08 15:45:37 -0600

    feat: lets keep node 10 as the default image for now

--- publish.sh
+++ publish.sh
@@ -17,26 +17,26 @@ if [[ -n "$GIT_STATUS" ]]; then
 fi
 
 echo "Will build images with the following tags:"
-echo -e "Based on node:12-alpine:    $NODE12_TAG, latest, $GIT_SHORT_COMMIT, $GIT_SHORT_COMMIT-$NODE12_TAG"
-echo -e "Based on node:10-alpine:    $NODE10_TAG, $GIT_SHORT_COMMIT-$NODE10_TAG"
+echo -e "Based on node:12-alpine:    $NODE12_TAG, $GIT_SHORT_COMMIT-$NODE12_TAG"
+echo -e "Based on node:10-alpine:    $NODE10_TAG, latest, $GIT_SHORT_COMMIT, $GIT_SHORT_COMMIT-$NODE10_TAG"
 echo -e "Based on node:8-alpine:     $NODE8_TAG, $GIT_SHORT_COMMIT-$NODE8_TAG"
 
 echo
 
-echo "Based on node:12-alpine (default image):"
+echo "Based on node:12-alpine:"
 
 docker build -t $REPO_NAME:$NODE12_TAG \
-  -t $REPO_NAME:latest \
-  -t $REPO_NAME:$GIT_SHORT_COMMIT \
   -t $REPO_NAME:$GIT_SHORT_COMMIT-$NODE12_TAG \
   -f ./node12/Dockerfile \
   .
 
 echo
 
-echo "Based on node:10-alpine:"
+echo "Based on node:10-alpine (default image):"
 
 docker build -t $REPO_NAME:$NODE10_TAG \
+  -t $REPO_NAME:latest \
+  -t $REPO_NAME:$GIT_SHORT_COMMIT \
   -t $REPO_NAME:$GIT_SHORT_COMMIT-$NODE10_TAG \
   -f ./node10/Dockerfile \
   .
@@ -55,10 +55,10 @@ echo
 echo "Pushing images to docker hub:"
 
 docker push $REPO_NAME:$NODE12_TAG
-docker push $REPO_NAME:latest
-docker push $REPO_NAME:$GIT_SHORT_COMMIT
 docker push $REPO_NAME:$GIT_SHORT_COMMIT-$NODE12_TAG
 docker push $REPO_NAME:$NODE10_TAG
+docker push $REPO_NAME:latest
+docker push $REPO_NAME:$GIT_SHORT_COMMIT
 docker push $REPO_NAME:$GIT_SHORT_COMMIT-$NODE10_TAG
 docker push $REPO_NAME:$NODE8_TAG
 docker push $REPO_NAME:$GIT_SHORT_COMMIT-$NODE8_TAG
