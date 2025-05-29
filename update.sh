1d1407496f34789f1d8e3d3c81bdf65a2fe6831c
Dan Shugan <dans@carimus.com>
2025-05-29 16:06:56 -0400

    Added support for node22. Added docker-cli as an installed package. Switched to using apk packages instead of pip packages. Intentionally departed from the template Dockerfile to avoid downstream dependency issues for projects still relying on node 8/10.

--- update.sh
+++ update.sh
@@ -4,6 +4,8 @@ set -e
 
 mkdir -p ./node8/
 mkdir -p ./node10/
+#mkdir -p ./node22/
 
 sed s/__BASE_IMAGE__/node:8-alpine/ < ./Dockerfile.template > ./node8/Dockerfile
 sed s/__BASE_IMAGE__/node:10-alpine/ < ./Dockerfile.template > ./node10/Dockerfile
+#sed s/__BASE_IMAGE__/node:22-alpine/ < ./Dockerfile.template > ./node22/Dockerfile
